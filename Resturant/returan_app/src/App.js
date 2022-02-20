95301c10856fc06f1db2e786c7cf2a7c2738e15d
ASADUJJAMANCODELIFE <67199735+ASADUJJAMANCODELIFE@users.noreply.github.com>
2022-02-20 20:12:12 +0600

    commit

---
+++ Resturant/returan_app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import resturant from "./component/Basics/resturant";
+const App = () => {
+
+  return (
+    <div>
+  <resturant />
+
+    </div>
+    
+  )
+
+};
+
+export default App;
---
+++ Resturant/returan_app/src/component/Basics/resturant.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const resturant = () => {
+  return (
+    <>
+ <div className='card-container'>
+   <div className='card'>
+     <div className='card-body'>
+       <span className='card-number card-circle subltle'>1</span>
+       <spna className='card-author subtle'></spna>
+
+     </div>
+
+   </div>
+ </div>
+    </>
+  )
+}
+
+export default resturant
